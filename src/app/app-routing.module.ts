ef601ef4c16c929770b85da8e611c1e244920b5d
Engin Can Çağlar <engincancaglar1998@gmail.com>
2023-12-29 01:57:44 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorComponent } from './error/error.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {path: '', component: DashboardComponent},
+      {path: 'about', component:AboutComponent},
+      {path: '404', component:ErrorComponent},
+      {path: '**', redirectTo: '404'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
